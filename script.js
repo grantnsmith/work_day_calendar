5cc88e35adb93a63b58162be5cf8196b81acc874
Grant Smith <grantnsmith@gmail.com>
2020-04-01 16:56:16 -0700

    Updated forms with ID's, pseudocoded rendering of page based on time.

--- script.js
+++ script.js
@@ -17,20 +17,28 @@ function saveEdits() {
 
 // Save Event button
 $(".saveBtn").on("click", function() {
-    // console.log("Button clicked");
+    console.log("Button clicked");
     saveEdits();
 });
 
 // Setting interval to run checkTime function every 10 seconds
 setInterval(function(){
-    if (moment().minute() == 7) {
-        console.log("it works");
+    if (moment().minute() == 0) {
+        renderScreen();
     }
 }, 10000);
 
+function renderScreen() {
+    // $("#5pm").addClass("table-success");
 
 
+    // Whatever the current hour (momement().hour()) is (e.g. 9,10,15,) find the 
+    // element with that ID and change this attribue (addClass "table-danger")
+    // Those with id's that are less than (addClass - "table-secondary")
+    // Those with id's that are greater than (addClass - "table-success")
+}
 
+// renderScreen();
 
 });
 
