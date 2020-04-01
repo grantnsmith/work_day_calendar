033f2494d09145b21ae0cc3bd6ff70906331873b
Grant Smith <grantnsmith@gmail.com>
2020-04-01 16:13:06 -0700

    Updated forms inside table cells away from bootstrap, added day to Jumbotron, added function to check time every ten seconds and to check for the changing of the hour.

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 $(document).ready(function() {
 
 // Adding current date to the top of the page
-var currentDate = moment().format('MMMM Do YYYY');
+var currentDate = moment().format('dddd MMMM Do YYYY');
 $("#currentDay").text(currentDate);
 
+// Add function to check localStorage for userEdits and render them to the page
 
+
+// Saving user edits
+function saveEdits() {
+    var userInput = document.getElementsByClassName("eventCol");
+    var userInput = eventFormEl.innerHTML;
+    localStorage.setItem("userEdits", JSON.stringify(userInput));
+    console.log("UserInput: " + userInput);
+}
+
+// Save Event button
+$(".saveBtn").on("click", function() {
+    // console.log("Button clicked");
+    saveEdits();
+});
+
+// Setting interval to run checkTime function every 10 seconds
+setInterval(function(){
+    if (moment().minute() == 7) {
+        console.log("it works");
+    }
+}, 10000);
 
 
 
@@ -13,4 +35,3 @@ $("#currentDay").text(currentDate);
 });
 
 
-// To change colors in table us class="table-danger" (red); "table-success" (green) or "table-secondary" (grey) "
